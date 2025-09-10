fd8cd47ef7185384070e1442bf0c9386b2ad5381
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2025-09-10 13:49:42 -0300

    Modifico Perfil.jsx y Perfil.css

--- capydo/src/components/Perfil.jsx
+++ capydo/src/components/Perfil.jsx
@@ -11,6 +11,8 @@ import GitHubIcon from '../assets/icons/social/github.svg';
 import LinkedInIcon from '../assets/icons/social/linkedin.svg';
 import WhatsappIcon from '../assets/icons/social/whatsapp.svg';
 
+import EditarIcon from '../assets/icons/editar.svg'
+
 const Perfil = () => {
 const [loaded, setLoaded] = useState(false);
 const [location, setLocation] = useState({
@@ -22,12 +24,15 @@ const [location, setLocation] = useState({
 
 const [educationLevel] = useState("Superior");
 const [institutionName] = useState("Universidad Nacional de San Rafael");
-const [bibliography] = useState(
+const [biography] = useState(
     'Aquí va tu biografía.'
 );
 
 const [fullName] = useState("Nombre y Apellido");
 const [role] = useState("Rol profesional");
+
+const [userName] = useState("nombreUsuario");
+const [mail] = useState("nombre@mail.com");
 
 useEffect(() => {
     const timer = setTimeout(() => {
@@ -72,20 +77,17 @@ useEffect(() => {
 
     return (
     <div className="app">
-        <div className="container">
+        <div className="container-profile-card">
             <div className={`profile-card ${loaded ? 'loaded' : ''}`}>
-                {/* Header con foto y botón */}
+                {/* Header con foto */}
                 <div className="profile-header">
                     <img src='../src/assets/fotosPerfil/fotoPerfil8.png' alt="" className="profile-image"/>
-                    <button className="edit-btn">Editar Perfil</button>
                 </div>
 
-                {/* Nombre, Rol y Bibliografía */}
-                <div className="profile-details bio-block">
+                {/* Nombre y Rol */}
+                <div className="profile-details">
                     <h1 className="profile-name">{fullName}</h1>
                     <p className="profile-title">{role}</p>
-                    <h4>Bibliografía</h4>
-                    <p>{bibliography}</p>
                 </div>
 
                 {/* Otros detalles */}
@@ -98,64 +100,81 @@ useEffect(() => {
                         </div>
                 </div>
 
-<div className="detail-item">
-    <img src={InstitucionIcon} alt="Institución Educativa" className="detail-icon" />
-    <div className="detail-content">
-        <h4>Institución</h4>
-        <p>{institutionName}</p>
-    </div>
-</div>
-<div className="detail-item">
-    <img src={UbicacionIcon} alt="Ubicación Actual" className="detail-icon" />
-        <div className="detail-content">
-            <h4>Ubicación Actual</h4>
-            {location.loaded ? (
-            location.city ? (
-            <p>{location.city}, {location.country}</p>
-            ) : (
-            <p>No se pudo detectar la ubicación</p>
-            )
-            ) : (
-            <p>Detectando ubicación...</p>
-            )}
+        <div className="detail-item">
+            <img src={InstitucionIcon} alt="Institución Educativa" className="detail-icon" />
+            <div className="detail-content">
+                <h4>Institución</h4>
+                <p>{institutionName}</p>
+            </div>
         </div>
-    </div>
-</div>
+        <div className="detail-item">
+            <img src={UbicacionIcon} alt="Ubicación Actual" className="detail-icon" />
+                <div className="detail-content">
+                    <h4>Ubicación Actual</h4>
+                    {location.loaded ? (
+                    location.city ? (
+                    <p>{location.city}, {location.country}</p>
+                    ) : (
+                    <p>No se pudo detectar la ubicación</p>
+                    )
+                    ) : (
+                    <p>Detectando ubicación...</p>
+                    )}
+                </div>
+            </div>
+        </div>
 
-{/* Redes sociales */}
-    <div className="social-links">
-        <a href="#" className="social-link discord">
-            <img src={DiscordIcon} alt="Discord" className="social-icon" />
-        </a>
-        <a href="#" className="social-link github">
-            <img src={GitHubIcon} alt="GitHub" className="social-icon" />
-        </a>
-        <a href="#" className="social-link linkedin">
-            <img src={LinkedInIcon} alt="LinkedIn" className="social-icon" />
-        </a>
-        <a href="#" className="social-link whatsapp">
-            <img src={WhatsappIcon} alt="WhatsApp" className="social-icon" />
-        </a>
-    </div>
+        {/* Redes sociales */}
+            <div className="social-links">
+                <a href="#" className="social-link discord">
+                    <img src={DiscordIcon} alt="Discord" className="social-icon" />
+                </a>
+                <a href="#" className="social-link github">
+                    <img src={GitHubIcon} alt="GitHub" className="social-icon" />
+                </a>
+                <a href="#" className="social-link linkedin">
+                    <img src={LinkedInIcon} alt="LinkedIn" className="social-icon" />
+                </a>
+                <a href="#" className="social-link whatsapp">
+                    <img src={WhatsappIcon} alt="WhatsApp" className="social-icon" />
+                </a>
+            </div>
 
-{/* Stats */}
-    <div className="profile-stats">
-        <div className="stat">
-                <span className="stat-number">2</span>
-                <span className="stat-label"> Proyectos <br/>Realizados</span>
+        {/* Stats */}
+            <div className="profile-stats">
+                <div className="stat">
+                        <span className="stat-number">2</span>
+                        <span className="stat-label"> Proyectos <br/>Realizados</span>
+                </div>
+                <div className="stat">
+                    <span className="stat-number">5</span>
+                    <span className="stat-label"> Certificaciones </span>
+                </div>
+                <div className="stat">
+                    <span className="stat-number">1</span>
+                    <span className="stat-label">Grupos</span>
+                </div>
+            </div>
+            </div>
         </div>
-        <div className="stat">
-            <span className="stat-number">5</span>
-            <span className="stat-label"> Certificaciones </span>
-        </div>
-        <div className="stat">
-            <span className="stat-number">1</span>
-            <span className="stat-label">Grupos</span>
+
+        <div className="container-biography">
+            {/* Botón, Biografía, Usuario y Mail */}
+            <div className="profile-info">
+                <h1 className="profile-username">@{userName}</h1>
+                <p className="profile-mail">{mail}</p>
+                <button className="edit-btn">
+                    <img src={EditarIcon} alt="Editar" className="editar-btn" />
+                    Editar Perfil
+                    </button>
+            </div>
+            
+            <div className="bio-block">
+                    <h4>Biografía</h4>
+                    <p>{biography}</p>
+            </div>
         </div>
     </div>
-    </div>
-    </div>
-</div>
 );
 };
 
